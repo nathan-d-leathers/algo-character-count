7c1df9933bb37c079cd4c266831a4de6ba128f91
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-05-30 13:40:29 -0700

    partially solved in js & python. couldnt sort alphabetically when char count was the same

--- js/charCount.js
+++ js/charCount.js
@@ -1,3 +1,21 @@
-exports.charCount = function() {
+exports.charCount = function(str) {
+    let repStr = str.replace(/ /g, "");
+    let charArr = repStr.split("").sort();
+    let letterMap = {};
+    let mappedArr = [];
+    
+    charArr.forEach(function charMap(char) {
+      letterMap[char] = (letterMap[char || 0]) +1;
+    });
 
-};
+    for (let letter in letterMap) {
+      mappedArr.push([letter, letterMap[letter]]);
+    };
+
+    mappedArr.sort(function(a,b) {
+      return b[1] - a[1];
+    });
+
+    return mappedArr;
+
+}
--- python/char_count.py
+++ python/char_count.py
@@ -1,2 +1,14 @@
 def char_count(str):
   # Your code here
+  new_list = []
+  for x in str:
+    count = str.count(x)
+    if [x,count] not in new_list:
+      new_list.append([x, count])
+  new_list.sort(key=lambda x:x[1],reverse=True)
+  return new_list
+
+
+#   -=-=-=-=- new_list.sort(key=lambda x:x[1],reverse=True) -=-=-=-=-
+#   good for reverse sorting a list by the second element, 
+#   however it does not sort the list alphabeticaly for the same number of elements
